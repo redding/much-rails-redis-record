a5873a83166cdcdb070c9581053da199fb722db7
Kelly Redding <kelly@kellyredding.com>
2021-06-16 17:07:04 -0500

    gem generated with ggem

---
+++ lib/much-rails-redis-record.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "much-rails-redis-record/version"
+
+module MuchRailsRedisRecord
+  # TODO: your code goes here...
+end
---
+++ lib/much-rails-redis-record/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MuchRailsRedisRecord
+  VERSION = "0.0.1"
+end
